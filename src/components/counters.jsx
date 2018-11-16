97e99995168d245a19825237b0ad26207115298b
Spencer Markin <markinspencer@gmail.com>
2018-11-15 23:06:32 -0500

    convert the counters class to a stateless functional component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./counter";
 
-class Counters extends Component {
-  render() {
-    const { onChange, onDelete, onReset, counters } = this.props;
-
-    return (
-      <div>
-        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
-          Reset
-        </button>
-        {counters.map(counter => (
-          <Counter
-            key={counter.id}
-            onDelete={onDelete}
-            onChange={onChange}
-            counter={counter}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Counters = ({ onChange, onDelete, onReset, counters }) => {
+  return (
+    <div>
+      <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+        Reset
+      </button>
+      {counters.map(counter => (
+        <Counter
+          key={counter.id}
+          onDelete={onDelete}
+          onChange={onChange}
+          counter={counter}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Counters;
